8f3544daa420cc3f01dc655294446bc4d36629e8
lcy362 <lcy362@163.com>
2017-02-16 19:31:32 +0800

    leetcode26

---
+++ src/main/java/com/mallow/algorithm/RemoveDuplicateArray.java
@@ -0,0 +1,21 @@
+package com.mallow.algorithm;
+
+/**
+ * leetcode26 Remove Duplicates from Sorted Array
+ * https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+ * Created by lcy on 2017/2/16.
+ */
+public class RemoveDuplicateArray {
+    public int removeDuplicates(int[] nums) {
+        if (nums.length <= 1) {
+            return nums.length;
+        }
+        int pos = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i-1]) {
+                nums[pos++] = nums[i];
+            }
+        }
+        return pos;
+    }
+}
