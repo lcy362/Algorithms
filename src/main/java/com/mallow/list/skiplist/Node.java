85ac71f89a9ad316b00c46971fcd8d1ea311c604
lcy362 <lcy362@163.com>
2017-05-09 18:36:36 +0800

    skiplist node

---
+++ src/main/java/com/mallow/list/skiplist/Node.java
@@ -0,0 +1,18 @@
+package com.mallow.list.skiplist;
+
+import lombok.Data;
+import lombok.Getter;
+
+import java.util.List;
+
+/**
+ * Created by lcy on 2017/5/9.
+ */
+@Getter
+public class Node {
+    private int value;
+    public List<Node> nextNodes;
+    public Node(int value) {
+        this.value = value;
+    }
+}
